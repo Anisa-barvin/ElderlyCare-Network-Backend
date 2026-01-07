2860c2ad7e562da73a5cab057a6fadd142c5d666
Anisa-barvin <barvinanisa@gmail.com>
2026-01-07 11:49:31 +0530

    Fix api key

--- utils/api.ts
+++ utils/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: "https://elder-network-backend.onrender.com/api", // 👈 IMPORTANT
+  baseURL: "https://elderlycare-network-backend.onrender.com/api", // 👈 IMPORTANT
 });
 
 export default api;
